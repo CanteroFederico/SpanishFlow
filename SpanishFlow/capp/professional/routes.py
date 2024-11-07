15cbf73a1aaed79e1b2b8c81c14649816832dff7
CanteroFederico <canterof@usal.es>
2024-11-07 13:13:26 +0100

    Delete SpanishFlow directory

--- SpanishFlow/capp/professional/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-professional=Blueprint('professional',__name__)
-
-
-@professional.route('/professional')
-def professional_home():
-    return render_template('professional.html', title= 'professional')
--- SpanishFlow/capp/student/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-student=Blueprint('student',__name__)
-
-
-@student.route('/student')
-def student_home():
-    return render_template('student.html', title= 'student')
