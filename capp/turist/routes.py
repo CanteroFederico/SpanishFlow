3770deba14d1467afa67ff85833f0c3ee57e44c0
CanteroFederico <canterof@usal.es>
2024-11-18 17:55:53 +0100

    Delete capp directory

--- capp/turist/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-turist=Blueprint('turist',__name__)
-
-
-@turist.route('/turist')
-def turist_home():
-    return render_template('turist.html', title= 'turist')
